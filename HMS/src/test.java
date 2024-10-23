fb7472db4100dbf695d6615bed0d4a8713e938a6
jingjieong <jingjieong@yahoo.com.sg>
2024-10-23 20:46:47 +0800

    v tired

--- HMS/src/test.java
+++ HMS/src/test.java
@@ -4,13 +4,14 @@ import java.io.IOException;
 public class test {
 
 	public static void main(String[] args) throws FileNotFoundException, IOException {
-		PatientListWriter writing=new PatientListWriter("C:\\\\Users\\\\jingj\\\\Desktop\\\\Code\\\\SC2002-OOP-Assignment\\\\Patient_List.xlsx");
+		DataWriter writing=new PatientListWriter("C:\\\\Users\\\\jingj\\\\Desktop\\\\Code\\\\SC2002-OOP-Assignment\\\\Patient_List.xlsx");
 		// x to set the column(column 0 is at the left)
 		//y to set row(row 0 is at the top)
-		writing.write(7,2,"smallcock");
-		PatientListReader reading=new PatientListReader("C:\\\\Users\\\\jingj\\\\Desktop\\\\Code\\\\SC2002-OOP-Assignment\\\\Patient_List.xlsx");
+		writing.write(7,2,"lol");
+		DataReader reading=new PatientListReader("C:\\\\Users\\\\jingj\\\\Desktop\\\\Code\\\\SC2002-OOP-Assignment\\\\Patient_List.xlsx");
 		reading.read();
-
+		DataReader readmeds=new MedicineListReader("C:\\\\Users\\\\jingj\\\\Desktop\\\\Code\\\\SC2002-OOP-Assignment\\\\Medicine_List.xlsx");
+		readmeds.read();
 	}
 
 }

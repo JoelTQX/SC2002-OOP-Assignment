e523bf3b3783781161b74ef9269575f894d114cd
jingjieong <jingjieong@yahoo.com.sg>
2024-10-31 16:52:55 +0800

    added doctor menu

---
+++ HospitalManagementSystem/src/viewers/AdministratorView.java
@@ -0,0 +1,10 @@
+package viewers;
+import java.util.Scanner;
+
+import controllers.DoctorController;
+public class AdministratorView implements ViewInteraface{
+	private AdministratorController admminControl;
+	private Scanner inputScanner;
+	
+
+}
--- HospitalManagementSystem/src/viewers/DoctorView.java
+++ HospitalManagementSystem/src/viewers/DoctorView.java
@@ -15,9 +15,43 @@ public class DoctorView implements ViewInterface{
 
 	@Override
 	public boolean displayMenu() {
-		System.out.println("--- Doctor Menu ---");
-		System.out.println("?. Exit ");
-		return false;
+		System.out.println("Doctor Menu:");
+        System.out.println("(1) View Patient Medical Records");
+        System.out.println("(2) Update Patient Medical Records");
+        System.out.println("(3) View Personal Schedule");
+        System.out.println("(4) Set Availability for Appointments");
+        System.out.println("(5) Accept or Decline Appointment Requests");
+        System.out.println("(6) View Upcoming Appointments");
+        System.out.println("(7) Record Appointment Outcome");
+        System.out.println("(8) Logout");
+        
+        int userChoice = inputScanner.nextInt();
+		
+		switch(userChoice){
+			case 1: 
+				//viewMedicalRecord();
+				break;
+			case 2:
+				//updatePersonalInformation();
+				break;
+			case 3: 
+				//viewAvailableSlots();
+				break;
+		}
+		if(userChoice == 8) return false;
+		else return true;
+			//viewMedicalRecord();
+		//else if(userChoice == 8) return false;
+		//return true;
+	}
+        
+        
+        
+        
+        
+        
+        
+        
 	}
 
-}
+

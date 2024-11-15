9317f32da67877e1d073c56b0e2c6b47deb557dc
Auwa <aurapasta@gmail.com>
2024-11-16 01:00:12 +0800

    Fixed some bugs

--- HospitalManagementSystem/src/viewers/PharmacistView.java
+++ HospitalManagementSystem/src/viewers/PharmacistView.java
@@ -83,12 +83,16 @@ public class PharmacistView implements ViewInterface{
         	}catch(Exception e) {
         		System.out.println("Invalid Format... Please Input An Integer");
         		inputScanner.next(); //Clear Buffer;
+        		continue;
         	}
         	if(userChoice > completedAppointments.size()) {
+        		if(userChoice == completedAppointments.size()+1) break;
         		System.out.println("Invalid Option... Please Try Again...");
+        		continue;
         	}
         	Appointment chosenAppointment = completedAppointments.get(userChoice-1);
         	viewAppointmentDetails(chosenAppointment);
+        	return;
         }while(userChoice != completedAppointments.size());
         System.out.println("Returning to menu...");
     }
@@ -116,8 +120,8 @@ public class PharmacistView implements ViewInterface{
 	 private void updatePrescriptionStatus() {
 		System.out.println("------ Update Prescription Status ------");
         System.out.print("Enter Appointment ID: ");
-        String appointmentId = inputScanner.next();
-        pharmacistControl.updatePrescriptionStatus(appointmentId);
+        String appointmentID = inputScanner.next();
+        pharmacistControl.updatePrescriptionStatus(appointmentID);
     }
 
 	private void submitReplenishment() {

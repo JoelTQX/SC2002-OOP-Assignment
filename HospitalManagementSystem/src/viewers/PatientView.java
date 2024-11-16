cc84fa52665a03779e6019cf667c85cd3d268763
frieddeli <130236946+frieddeli@users.noreply.github.com>
2024-11-16 14:32:32 +0800

    Enhance PatientView to display Doctor ID alongside appointment details

--- HospitalManagementSystem/src/viewers/PatientView.java
+++ HospitalManagementSystem/src/viewers/PatientView.java
@@ -126,6 +126,7 @@ public class PatientView implements ViewInterface{
 		} else {
 			for (Appointment appointment : availableSlots) {
 				System.out.println("Appointment ID: " + appointment.getAppointmentID() +
+							   ", Doctor ID: " + appointment.getDoctorId() +
 							   ", Date: " + appointment.getAppointmentDate() +
 							   ", Time: " + appointment.getAppointmentTime() +
 							   ", Status: " + appointment.getStatus() );

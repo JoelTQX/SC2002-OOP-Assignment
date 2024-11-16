96b5bcd769a5079d4c1e54c57733eaf5c064a539
Auwa <aurapasta@gmail.com>
2024-11-16 15:52:20 +0800

    Cleaned up unused imports

--- HospitalManagementSystem/src/entities/Pharmacist.java
+++ HospitalManagementSystem/src/entities/Pharmacist.java
@@ -1,7 +1,5 @@
 package entities;
 
-import datastorage.Inventory;
-
 public class Pharmacist extends Staff{
 	
 	public Pharmacist(String userID,String userName, String userPass, boolean firstLogin, String userRole, String gender, int age) {

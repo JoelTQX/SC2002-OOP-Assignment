5ca63e4b5a2a15286448cf23887fe58f0ec07c3e
JoelTQX <joeltan135246@gmail.com>
2024-11-17 23:38:32 +0800

    Javadoc for DataStorage

--- HospitalManagementSystem/src/datastorage/AppointmentRecords.java
+++ HospitalManagementSystem/src/datastorage/AppointmentRecords.java
@@ -7,18 +7,35 @@ import java.util.List;
 import entities.Appointment;
 import entities.Appointment.AppointmentStatus;
 
+/**
+ * The `AppointmentRecords` class manages the list of appointments and provides various methods
+ * to retrieve, modify, and manage appointments, such as getting available slots, retrieving appointments
+ * by status, and adding or removing appointments.
+ */
 public class AppointmentRecords {
+    /**
+     * The last generated appointment ID. Used to assign a unique ID to each new appointment.
+     */
     static int lastAppointmentID = 0;
+    
+    /**
+     * List of all appointments.
+     */
     private List<Appointment> AppointmentRecords; // holds a list of appointments
 
+    /**
+     * Constructs an empty list of appointment records.
+     */
     public AppointmentRecords() {
         AppointmentRecords = new ArrayList<Appointment>();
     }
 
-    /////////////////////////////////////////////////////////////////////////////////////////////////
-    /// APPOINTMENT RETRIEVAL METHODS ///////////////////////////////////////////////////////////////
-
-    // Method to get an appointment by its ID
+    /**
+     * Retrieves an appointment by its unique appointment ID.
+     *
+     * @param appointmentID The appointment ID to search for.
+     * @return The appointment matching the given ID, or null if not found.
+     */
     public Appointment getAppointmentByID(String appointmentID) {
         for (Appointment appointment : AppointmentRecords) {
             if (appointment.getAppointmentID().equals(appointmentID)) {
@@ -28,17 +45,30 @@ public class AppointmentRecords {
         return null; // Appointment Not Found
     }
 
-    // Method to get the last appointment ID
+    /**
+     * Retrieves the last used appointment ID.
+     *
+     * @return The last appointment ID.
+     */
     public int getLastAppointmentID() {
         return lastAppointmentID;
     }
 
-    // Method to get the full list of appointments
+    /**
+     * Retrieves the full list of all appointments.
+     *
+     * @return A list of all appointments.
+     */
     public List<Appointment> getFullAppointmentList() {
         return AppointmentRecords;
     }
 
-    // Method to get appointments for a specific date
+    /**
+     * Retrieves all appointments for a specific date.
+     *
+     * @param date The date for which to retrieve appointments.
+     * @return A list of appointments on the given date.
+     */
     public List<Appointment> getAppointmentsForDate(String date) {
         List<Appointment> appointmentsForDate = new ArrayList<>();
         for (Appointment appointment : AppointmentRecords) {
@@ -48,8 +78,12 @@ public class AppointmentRecords {
         }
         return appointmentsForDate;
     }
-
-    // Method to get available appointment slots for a specific day
+    /**
+     * Retrieves a list of available appointment slots for a specific date.
+     *
+     * @param date The date for which to retrieve available slots.
+     * @return A list of available appointment time slots.
+     */
     public List<String> getEmptySlots(String date) {
         List<String> allSlots = List.of(
             "09:00", "10:00", "11:00", "12:00", "13:00", "14:00", "15:00", "16:00", "17:00"
@@ -64,7 +98,11 @@ public class AppointmentRecords {
         return emptySlots;
     }
 
-    // Method to get all available slots
+    /**
+     * Retrieves all available appointment slots in the system.
+     *
+     * @return A list of available appointments with status AVAILABLE.
+     */
     public List<Appointment> getALLSlots() {
         List<Appointment> availableSlots = new ArrayList<>();
         for (Appointment appointment : AppointmentRecords) {
@@ -75,7 +113,12 @@ public class AppointmentRecords {
         return availableSlots;
     }
 
-    // Method to get available slots for a specific date
+    /**
+     * Retrieves available appointment slots for a specific date.
+     *
+     * @param date The date for which to retrieve available slots.
+     * @return A list of available appointments on the given date.
+     */
     public List<Appointment> getSlotsForDate(String date) {
         List<Appointment> availableSlots = new ArrayList<>();
         for (Appointment appointment : AppointmentRecords) {
@@ -87,7 +130,12 @@ public class AppointmentRecords {
         return availableSlots;
     }
 
-    // Method to retrieve scheduled appointments for a patient
+    /**
+     * Retrieves scheduled appointments for a specific patient based on their patient ID.
+     *
+     * @param patientId The patient ID for which to retrieve scheduled appointments.
+     * @return A list of scheduled appointments for the given patient.
+     */
     public List<Appointment> getScheduledAppointments(String patientId) {
         List<Appointment> scheduledAppointments = new ArrayList<>();
         for (Appointment appointment : AppointmentRecords) {
@@ -98,7 +146,12 @@ public class AppointmentRecords {
         return scheduledAppointments;
     }
 
-    // Method to retrieve completed appointments for a patient
+    /**
+     * Retrieves completed appointments for a specific patient based on their patient ID.
+     *
+     * @param patientId The patient ID for which to retrieve completed appointments.
+     * @return A list of completed appointments for the given patient.
+     */
     public List<Appointment> getCompletedAppointments(String patientId) {
         List<Appointment> completedAppointments = new ArrayList<>();
         for (Appointment appointment : AppointmentRecords) {
@@ -109,7 +162,12 @@ public class AppointmentRecords {
         return completedAppointments;
     }
 
-    // Method to find all appointments for a specific patient ID
+    /**
+     * Retrieves appointments for a specific patient based on their patient ID.
+     *
+     * @param patientId The patient ID for which to retrieve appointments.
+     * @return A list of appointments for the given patient.
+     */
     public List<Appointment> findAppointmentsByPatientId(String patientId) {
         List<Appointment> patientAppointments = new ArrayList<>();
         for (Appointment appointment : AppointmentRecords) {
@@ -120,7 +178,13 @@ public class AppointmentRecords {
         return patientAppointments;
     }
 
-    // Method to get appointments for a doctor based on status
+    /**
+     * Retrieves doctor appointments based on the doctor's user ID and the appointment status.
+     *
+     * @param userId The doctor’s user ID.
+     * @param status The appointment status.
+     * @return A list of appointments for the doctor with the given status.
+     */
     public List<Appointment> getDocAppointments(String userId, AppointmentStatus status) {
         List<Appointment> pendingAppointments = new ArrayList<>();
         for (Appointment appointment : AppointmentRecords) {
@@ -131,7 +195,13 @@ public class AppointmentRecords {
         return pendingAppointments;
     }
 
-    // Method to get appointments for a patient based on status
+    /**
+     * Retrieves patient appointments based on the patient’s user ID and the appointment status.
+     *
+     * @param userId The patient’s user ID.
+     * @param status The appointment status.
+     * @return A list of appointments for the patient with the given status.
+     */
     public List<Appointment> getPatientAppointments(String userId, AppointmentStatus status) {
         List<Appointment> pendingAppointments = new ArrayList<>();
         for (Appointment appointment : AppointmentRecords) {
@@ -142,31 +212,44 @@ public class AppointmentRecords {
         return pendingAppointments;
     }
 
-    // Method to get all appointment records
+    /**
+     * Retrieves the list of all appointment records.
+     *
+     * @return A list of all appointments.
+     */
     public List<Appointment> getAppointmentRecords() {
         return this.AppointmentRecords;
     }
 
-    /////////////////////////////////////////////////////////////////////////////////////////////////
-    /// APPOINTMENT MODIFICATION METHODS ///////////////////////////////////////////////////////////
-
-    // Method to add an appointment
+    /**
+     * Adds a new appointment to the records and increments the last appointment ID.
+     *
+     * @param appointment The appointment to add to the records.
+     */
     public void addAppointment(Appointment appointment) {
         AppointmentRecords.add(appointment);
         lastAppointmentID++;
     }
 
-    // Method to remove an appointment
+    /**
+     * Removes a specific appointment from the records.
+     *
+     * @param appointment The appointment to remove from the records.
+     */
     public void removeAppointment(Appointment appointment) {
         AppointmentRecords.remove(appointment);
     }
 
 
 
-    /////////////////////////////////////////////////////////////////////////////////////////////////
-    /// HELPER METHODS //////////////////////////////////////////////////////////////////////////////
-
-    // Helper method to generate hourly slots from startTime to endTime for a specific date
+    /**
+     * Generates hourly slots for a given date within a start and end time range.
+     *
+     * @param date The date for which to generate slots.
+     * @param startTime The start time of the range (inclusive).
+     * @param endTime The end time of the range (inclusive).
+     * @return A list of time slots within the specified range for the given date.
+     */
     private List<String> generateHourlySlots(String date, String startTime, String endTime) {
         List<String> slots = new ArrayList<>();
         DateTimeFormatter timeFormatter = DateTimeFormatter.ofPattern("HH:mm");
--- HospitalManagementSystem/src/datastorage/Inventory.java
+++ HospitalManagementSystem/src/datastorage/Inventory.java
@@ -5,25 +5,48 @@ import java.util.List;
 
 import entities.Medicine;
 
+/**
+ * The `Inventory` class represents a collection of medicines and provides methods
+ * to manage the inventory, such as adding and removing medicines, viewing the inventory,
+ * and retrieving specific medicines by name.
+ */
 public class Inventory {
+    /**
+     * A list of medicine records stored in the inventory.
+     */
 	private List<Medicine> medicineRecords;
 	
-	//Constructor
+    /**
+     * Constructs an empty inventory.
+     */
 	public Inventory() {
 		this.medicineRecords = new ArrayList<Medicine>();
 	}
 	
-	//To add Medicine to Inventory
+    /**
+     * Adds a medicine to the inventory.
+     *
+     * @param medicine The medicine to be added to the inventory.
+     */
 	public void addMedicine(Medicine medicine) {
 		this.medicineRecords.add(medicine);
 	}
 	
-	//To remove Medicine from Inventory
+    /**
+     * Removes a medicine from the inventory.
+     *
+     * @param medicine The medicine to be removed from the inventory.
+     */
 	public void removeMedicine(Medicine medicine) {
 		this.medicineRecords.remove(medicine);
 	}
 	
-	//Get Medicine by Medicine Name
+    /**
+     * Retrieves a medicine from the inventory by its name.
+     *
+     * @param medicineName The name of the medicine to retrieve.
+     * @return The medicine object if found, or null if the medicine does not exist in the inventory.
+     */
 	public Medicine getMedicineByName(String medicineName) {
 		for( Medicine medicine : medicineRecords) {
 			if(medicine.getMedicineName().equals(medicineName)) {
@@ -33,7 +56,9 @@ public class Inventory {
 		return null; //If Medicine does not exist in Inventory
 	}
 	
-	//View Inventory
+    /**
+     * Displays the list of all medicines in the inventory along with their stock information.
+     */
 	public void viewInventory() {
 		System.out.println("------ Inventory -------");
 		for( Medicine medicine : medicineRecords) {
@@ -43,7 +68,11 @@ public class Inventory {
 		}
 	}
 	
-	// Return MedicineRecords
+    /**
+     * Returns the list of all medicines in the inventory.
+     *
+     * @return The list of medicine records.
+     */
 	public List<Medicine> getMedicineRecords() {
 		return this.medicineRecords;
 	}

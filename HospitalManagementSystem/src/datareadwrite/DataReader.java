2d237918b5d448b990be97a04af0204ad6f2c58f
jingjieong <jingjieong@yahoo.com.sg>
2024-11-19 17:14:32 +0800

    pushed java docs

--- HospitalManagementSystem/src/datareadwrite/DataReader.java
+++ HospitalManagementSystem/src/datareadwrite/DataReader.java
@@ -2,7 +2,7 @@ package datareadwrite;
 
 import java.io.IOException;
 import datastorage.DataStorage;
-/*
+/**
  * Interface {@code DataReader} provides a contract for classes that implement this interface.
  * Classes that implement {@code DataReader} must provide implementation for {@link #populateData()} method.
  * Classes implementing this interface must provide implementations for the following methods:
@@ -17,7 +17,18 @@ import datastorage.DataStorage;
  * 
  */
 public interface DataReader {
-	
+	/**
+     * Populates the given {@link datastorage.DataStorage} instance of objects.
+     *
+     * <p>
+     * Implementing classes should provide logic to read data from respective CSV 
+     *  
+     * {@link datastorage.DataStorage} object with the retrieved data.
+     * </p>
+     *
+     * @param dataStorage the {@code DataStorage} instance to populate with data
+     * @throws IOException if an I/O error occurs during data reading
+     */
 	public void populateData(DataStorage dataStorage) throws IOException;
 
 }
